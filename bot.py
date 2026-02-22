5d763c4ae6dc4a126980ea2a8e97c6b3de65927a
mrdjas88-art <mr.djas88@gmail.com>
2026-02-22 22:52:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -271,13 +271,14 @@ async def step_4_watched(call: types.CallbackQuery):
     )
     await call.answer()
 
-@dp.message_handler(content_types=types.ContentTypes.VIDEO)
-async def get_video_id(message: types.Message):
-    await message.answer(message.video.file_id)
+@dp.channel_post_handler(content_types=types.ContentTypes.VIDEO)
+async def get_channel_video_id(message: types.Message):
+    print(message.video.file_id)
 
 if __name__ == "__main__":
 
     executor.start_polling(dp, skip_updates=True)
+
 
 
 
